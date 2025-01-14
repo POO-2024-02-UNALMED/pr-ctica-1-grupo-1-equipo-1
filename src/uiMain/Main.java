6bde9d7c3fa2668876b8406f39396e8f04cc7e05
Santiago Salcedo <ssalcedor@unal.edu.co>
2025-01-13 23:45:20 -0500

    Creacion de clase Main
    Se creo la clase Main la cual tiene por ahora una variable que almacena el tiempo justo cuando ejecuta el programa

---
+++ src/uiMain/Main.java
@@ -0,0 +1,12 @@
+package uiMain;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+            LocalTime horaZero = Localtime.now();// Captura el tiempo al ejecutarse el programa
+
+
+
+    }
+}
